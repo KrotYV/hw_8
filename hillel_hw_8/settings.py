54196a726530344dd237d9c251e62366af81248d
Yulia <krot.y.v@gmail.com>
2021-08-26 16:40:39 +0300

    initial commit

--- hillel_hw_8/settings.py
+++ hillel_hw_8/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'hillel_hw_8.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
